5d3cae790415a11c005110199a12606cb550684d
Keerthivasan P <110258711+bigbang0431@users.noreply.github.com>
2022-10-04 08:20:09 +0530

    Create Python program for Raspberry pi.py

---
+++ Assessments/Team member 3-Kesavaraj K/Assignment 3/Python program for Raspberry pi.py
@@ -0,0 +1,37 @@
+For Led Blinking:
+ 
+import RPi.GPIO as GPIO 
+from time import sleep
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+
+while True: 
+ GPIO.output(8, GPIO.HIGH)
+ sleep(1) 
+ GPIO.output(8, GPIO.LOW) 
+ sleep(1)
+
+
+For Traffic Lights:
+
+import RPi.GPIO as GPIO 
+from time import sleep
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW) 
+
+while True: 
+ GPIO.output(8, GPIO.HIGH)
+ sleep(1) 
+ GPIO.output(8, GPIO.LOW) 
+GPIO.output(10, GPIO.HIGH) 
+ sleep(1)
+GPIO.output(10, GPIO.LOW) 
+GPIO.output(12, GPIO.HIGH)
+sleep(1)
+GPIO.output(12, GPIO.LOW)
